88815b56d5a7c540c986370c44b715e93c12c8d8
Angel Aranibar <angelaranibar@MacBook-Pro-de-Angel.local>
2020-06-24 12:17:27 -0400

    github

--- src/Controlador/HTTP/Post/peticionMovil.js
+++ src/Controlador/HTTP/Post/peticionMovil.js
@@ -5,13 +5,25 @@ module.exports = (rutas, bd, ver, datos, http, passport)=>
   rutas.post(http.post.rutaMovil.iniciarSesion,(req,res)=>
   {
     console.log(req.body)
-    if(req.body.username == "erick" && req.body.password == "contra")
-    {
-      res.json({if:1})
-    }
-    else {
-      res.json(req.body)
-    }
+    bd.cruds.crudUsuario.buscar({correo:{tipo: 'igual',valor: req.body.correo}}, (usuario)=>{
+     if((usuario.length<=0)){
+      return done(null, false, req.flash('error', 'El correo '+ correo + ' no está registrado'));
+     }
+     else
+     {
+       console.log('contra:',contra, usuario[0].contra)
+       bcrypt.compare(contra, usuario[0].contra, function(err, resp) {
+         if(err) console.log(err);
+           if(resp==true)
+           {
+             res.json({if:1,datos:usuario[0]})
+           }
+           else {
+             res.json(req.body)
+           }
+       });
+     }
+    });
   });
   rutas.get("/Movil/Prueba/" ,(req,res)=>
   {
