534040b43a554a0d5274619ba46514b6c3657520
Angel Aranibar <angelaranibar@MacBook-Pro-de-Angel.local>
2020-06-24 12:59:35 -0400

    github

--- src/Controlador/HTTP/Post/peticionMovil.js
+++ src/Controlador/HTTP/Post/peticionMovil.js
@@ -1,5 +1,6 @@
 const tarifa = "1";
 const fechas = require('./../Utiles/fechas');
+var bcrypt = require('bcryptjs');
 module.exports = (rutas, bd, ver, datos, http, passport)=>
 {
   rutas.post(http.post.rutaMovil.iniciarSesion,(req,res)=>
