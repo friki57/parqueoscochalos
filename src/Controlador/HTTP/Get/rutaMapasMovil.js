a4663d94fc172aabc21e7111d4bbc604aa0cb9f1
Angel Aranibar <angelaranibar@MacBook-Pro-de-Angel.local>
2020-06-26 11:13:27 -0400

    github

--- src/Controlador/HTTP/Get/rutaMapasMovil.js
+++ src/Controlador/HTTP/Get/rutaMapasMovil.js
@@ -1,5 +1,11 @@
 
 module.exports = (rutas, bd, ver, datos, http)=>
 {
-  
+  rutas.get("/MapasMovil/" ,(req,res)=>
+  {
+    bd.cruds.crudCalle.leer((cal)=>{
+
+      res.json(cal)
+    })
+  });
 }
