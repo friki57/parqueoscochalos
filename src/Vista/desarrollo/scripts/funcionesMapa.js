48734c31b0efed2134b7498e103a714200e9448a
Erick Aranibar <aranibarerick@gmail.com>
2021-12-19 16:26:11 -0400

    mapafinal

--- src/Vista/desarrollo/scripts/funcionesMapa.js
+++ src/Vista/desarrollo/scripts/funcionesMapa.js
@@ -1,4 +1,11 @@
 var base;
+function coordenadas(la,lo)
+{
+  this.lat=la;
+  this.lon=lo;
+}
+var posicion = new coordenadas();
+var map;
 $(document).ready(()=> {
   //iniciarMapa();iniciarBD();
   iniciarMapa();
@@ -9,21 +16,16 @@ $(document).ready(()=> {
   },300);
 });
 
-function coordenadas(la,lo)
-{
-  this.lat=la;
-  this.lon=lo;
-}
-var posicion = new coordenadas();
-var map;
+
 function iniciarMapa()
 {
 
   console.log("************bbbbbbbbbbbbbbbbbbb***********************")
     navigator.geolocation.getCurrentPosition(
         function (position) {
+          console.log(position);
           posicion.lat= position.coords.latitude;
-          posicion.lon=position.coords.longitude;
+          posicion.lon= position.coords.longitude;
           mapboxgl.accessToken = 'pk.eyJ1IjoiZnJpa2k1NyIsImEiOiJjanZxOGtxMjgwaDhxNDRvOHl5NDVvZnQyIn0._cULjNb2IP5SLSBSm7Higw';
           map = new mapboxgl.Map({
           container: 'mapa',
@@ -97,37 +99,47 @@ else {
 
 }
   if(GeoJson!=null)
+  {
+    var flon = (GeoJson[0].lon + GeoJson[1].lon)/2;
+    var flat = (GeoJson[0].lat + GeoJson[1].lat)/2;
+    const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(texto)
+    const marker1 = new mapboxgl.Marker({color:color})
+      .setLngLat([flon,flat])
+      .setPopup(popup)
+      .addTo(map);
+  }
+
+if(false){
   map.addLayer({
     "id": id2.toString(),
     "type": "line",
     "source": {
-    "type": "geojson",
-    "data": {
-    "type": "Feature",
-    "properties": {},
-    "geometry": {
-    "type": "LineString",
-    "coordinates":  [
-                      [GeoJson[0].lon,GeoJson[0].lat],
-                      [GeoJson[1].lon,GeoJson[1].lat]
-                  //   [-17.40123052219971, -66.16509592589807], [-17.399756270306867, -66.16644775924286], [-17.400022455550342, -66.16666554695834], [-17.402704762208103, -66.16514205223696], [-17.402827615008675, -66.16228818184796]
-                    ]
-                }
-            }
-        },
+      "type": "geojson",
+      "data": {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "LineString",
+          "coordinates":  [
+            [GeoJson[0].lon,GeoJson[0].lat],
+            [GeoJson[1].lon,GeoJson[1].lat]
+            //   [-17.40123052219971, -66.16509592589807], [-17.399756270306867, -66.16644775924286], [-17.400022455550342, -66.16666554695834], [-17.402704762208103, -66.16514205223696], [-17.402827615008675, -66.16228818184796]
+          ]
+        }
+      }
+    },
     "layout": {
-    "line-join": "round",
-    "line-cap": "round"
-    // <div id = "cabecera">
-    //   <button id="ubica">Ubicación</button>
-    //   </div>
+      "line-join": "round",
+      "line-cap": "round"
+      // <div id = "cabecera">
+      //   <button id="ubica">Ubicación</button>
+      //   </div>
     },
     "paint": {
-    "line-color": color,
-    "line-width": 8
+      "line-color": color,
+      "line-width": 8
     }
-    });
-
+  });
   map.on('click',id2.toString(),(e)=>
   {
     new mapboxgl.Popup()
@@ -135,18 +147,22 @@ else {
     .setHTML(texto)
     .addTo(map);
   });
-//map.setCenter([GeoJson[0].lon, GeoJson[0].lat]);
+
+}
+map.setCenter([GeoJson[0].lon, GeoJson[0].lat]);
 }
 var actual;
 function ubicarcentro() {
   //map.setCenter([-66.16376535943817,-17.372440167902013]);
+navigator.geolocation.getCurrentPosition(function () {}, function () {}, {});
   navigator.geolocation.getCurrentPosition(
       function (position) {
+        console.log(position);
         posicion.lat= position.coords.latitude;
         posicion.lon=position.coords.longitude;
         map.setCenter([posicion.lon,posicion.lat]);
         actual = new mapboxgl.Marker().setLngLat([posicion.lon,posicion.lat]).addTo(map);
-  });
+  },()=>console.log("fallo la geolocalización"),{maximumAge:60000, timeout:5000, enableHighAccuracy:true});
 
 }
 var capa = 1;
@@ -189,7 +205,6 @@ function eliminarCapa(GeoJson,id)
 }
 function alternarCapnombre()
 {
-  console.log("capnombre: ",capnombre)
   if(capnombre==true)
   {
   capnombre=false;
@@ -264,8 +279,10 @@ function recibirCalles(calles)
          return "<tr><td><div class = 'p-1 font-semibold' style = 'background-color:#0B313F; color: #E79A32' >"+a.placa+"</div><td>"
          + "<td><div class = 'p-2 " + colt +"' > queda: <span class='"+ calles[k].key +"'>"+ tiempopl +"</span> minuto(s)</div></td>"
        });
-       console.log(       calles[k].placas)
        placas.push("<table>" + calles[k].placas.join("") + "</table>");
+     }
+     else {
+       placas.push("")
      }
      if(calles[k].espacios != undefined)
       espacios.push(calles[k].espacios);
@@ -278,6 +295,7 @@ function recibirCalles(calles)
      var k = keys[i];
     // console.log(geojons[i],k);
     var textoPlaca = "No se encuentra ningún vehículo estaciondo en esta calle durante este momento";
+
     if(placas[i]!=undefined)
       textoPlaca = placas[i];
     var texto= "".concat("<div id='popup",k,"'>",nomb[i]," entre ",c1[i]," y ", c2[i], ' <br></br> Número de espacios disponibles: ', espaciosMaximo[i] - espacios[i],
