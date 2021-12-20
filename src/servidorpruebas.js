0dc7fb32b4db6b3d2ad4cf27d7b94d25555f68d3
Erick Aranibar <aranibarerick@gmail.com>
2021-12-19 21:17:31 -0400

    socket

--- src/servidorpruebas.js
+++ src/servidorpruebas.js
@@ -11,8 +11,8 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-var puerto = process.env.PORT || "4000";
-
+var puerto = "3000";
+// var puerto = process.env.PORT || "4000";
 require('./Modelo/Autenticacion/local.js')(passport);
 
 app.use(morgan('dev'));
