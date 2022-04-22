f7ce41fe4a772dd52b464b9ca22f806cbcf372b9
friki57 <aranibarerick@gmail.com>
2022-04-22 21:04:26 +0000

    github

--- src/parqueosCochalos.js
+++ src/parqueosCochalos.js
@@ -11,7 +11,7 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
-var puerto = "4000";
+var puerto = "5000";
 // var puerto = process.env.PORT || "4000";
 
 require('./Modelo/Autenticacion/local.js')(passport);
